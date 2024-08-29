3673d184ef1eee8d07a1d9ec471e9d30e635b490
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2024-08-29 10:32:09 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,25 +1,28 @@
 calls = 0
+
+
 def count_calls():
-   global calls
-   calls +=1
+    global calls
+    calls += 1
+
+
 def string_info(string):
     count_calls()
-    a = (len(string), string.upper(), string.lower() )
+    a = (len(string), string.upper(), string.lower())
     return (a)
+
+
 def is_contains(string, list_to_search):
     count_calls()
-    b = string
-    for i in list_to_search:
-         if b.lower() == i.lower():
-              return 'True'
-         else:
-             return 'False'
-         #print(i.lower())
-    #print(string.lower())
+    if any(string.lower() == i.lower() for i in list_to_search):
+        return 'True'
+    else:
+        return 'False'
+
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN', 'Urban']))  # Urban ~ urBAN
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 
 print(calls)
